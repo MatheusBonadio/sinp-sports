c1191ee2edf233c3651e7c4194f5cd0878c3af78
Bonadio <matheus.bonadio@etec.sp.gov.br>
2017-11-12 00:12:31 -0200

    Font face

--- errors/404.php
+++ errors/404.php
@@ -1,4 +1,6 @@
-    <link rel="stylesheet" href="/public/css/error.css" type="text/css">
+    <link rel="stylesheet" href="public/css/error.css" type="text/css">
     <div class='error'>
-        404
+        <label>404</label>
+        <div class='line'></div>
+        <label>é melhor sair daqui rápido, você não quer ficar fora do próximo jogo, não é?</label>
     </div>
